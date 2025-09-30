e33603a1af0b9ac4c3a4fc290043ebd772279679
kabir911 <kabir@divm.com>
2025-09-30 19:51:06 +0200

    Added mac notar

---
+++ scripts/notarize.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { notarize } = require('@electron/notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    tool: 'notarytool',
+    teamId: process.env.APPLETEAMID,
+    appBundleId: 'com.kabir911.lrag',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS,
+  });
+};
+
+
