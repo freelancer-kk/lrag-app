be33162981fda2ae33daf27effb818f3e916f4ef
Kabir Khaleque <kabir@divm.com>
2025-11-23 20:05:44 +0100

    disable restart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -729,9 +729,11 @@ export class AppComponent implements OnInit {
       /*
       * Comment out since only necessary for reload
       */
+     /*
       this.ollamaService.startOnTimer();
       this.watcherService.startIfNecessary();
       this.rerankerService.startIfNecessary();     
+      */
     }, 400)   
   }
 
