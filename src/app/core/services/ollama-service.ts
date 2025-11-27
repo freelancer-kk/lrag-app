5303b34fac6c35133d578b362d238bc0595f2624
Kabir Khaleque <kabir@divm.com>
2025-11-27 17:02:57 +0100

    typo

--- src/app/core/services/ollama-service.ts
+++ src/app/core/services/ollama-service.ts
@@ -22,10 +22,10 @@ export class OllamaService {
     {value: 'embeddinggemma:300m', viewValue: 'embeddinggemma:300m (<1GB)', thinking: false, cloud: false, memory: 1, description: 'EmbeddingGemma is a 300M parameter embedding model from Google'},    
   ]
   ocr_models: any[] = [
-    { value: 'benhaotang/Nanonets-OCR-s:latest', viewValue: 'Nanonets-OCR-s (<5GB>)', thinking: false, cloud: false, memory: 5, description: 'Nanonets OCR is a cloud-based optical character recognition model that provides accurate text extraction from images.', prompt: 'Perform Optical Character Recognition (OCR) on the provided image and format all extracted text as a clear, structured Markdown document. Include tables as markdown tables, lists as markdown lists, etc.',
+    { value: 'benhaotang/Nanonets-OCR-s:latest', viewValue: 'Nanonets-OCR-s (<5GB)', thinking: false, cloud: false, memory: 5, description: 'Nanonets OCR is a cloud-based optical character recognition model that provides accurate text extraction from images.', prompt: 'Perform Optical Character Recognition (OCR) on the provided image and format all extracted text as a clear, structured Markdown document. Include tables as markdown tables, lists as markdown lists, etc.',
         params: {}
     },
-    {value: 'deepseek-ocr:latest', viewValue: 'deepseek-ocr (8GB)', thinking: false, cloud: false, memory: 8, description: 'DeepSeek OCR is an advanced optical character recognition model designed to extract text from images with high accuracy.', prompt: '<|grounding|>Convert the document to markdown.',
+    {value: 'deepseek-ocr:latest', viewValue: 'deepseek-ocr (<8GB)', thinking: false, cloud: false, memory: 8, description: 'DeepSeek OCR is an advanced optical character recognition model designed to extract text from images with high accuracy.', prompt: '<|grounding|>Convert the document to markdown.',
       params: {
         "temperature": 0
       }
